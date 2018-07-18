d520053d1048141feb5d159ec5c5c579a4383636
Alexander Hofstätter <alex@hofstaetter.io>
2018-07-18 19:42:54 +0200

    added api to deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ git fetch --all
 git reset --hard origin/master
 git pull origin master
 
+rm -rf apis/wu-learn-api/
+git clone https://github.com/alexanderhofstaetter/wu-learn-api.git apis/wu-learn-api/
+chmod +x apis/wu-learn-api/*.py
+
 composer install
 php artisan migrate --force
 
@@ -13,3 +17,4 @@ if [ -e "storage/app/version" ]; then
 fi
 touch storage/app/version
 date +"%Y.%m.%d" > storage/app/version
+
