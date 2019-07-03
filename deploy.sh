e4d60b602376074c55165605ce931afe68a764ba
Alexander Hofstätter <alex@hofstaetter.io>
2019-07-03 15:43:29 +0200

    Updated to Laravel 5.8

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,13 @@ git fetch --all
 git reset --hard origin/master
 git pull origin master
 
+# get current versions of API dependencies
 rm -rf apis/wu-learn-api/
+rm -rf apis/wu-lpis-api/
 git clone https://github.com/alexanderhofstaetter/wu-learn-api.git apis/wu-learn-api/
+git clone https://github.com/alexanderhofstaetter/wu-lpis-api.git apis/wu-lpis-api/
 chmod +x apis/wu-learn-api/*.py
+chmod +x apis/wu-lpis-api/*.py
 
 composer install
 php artisan migrate --force
